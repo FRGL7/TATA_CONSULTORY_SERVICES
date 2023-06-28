3d986d046cad6f4662c2287a7e0d860bbc12acb9
FRGL7 <rogofe7@hotmil.com>
2023-06-28 17:46:57 -0600

    firts example worked with spring batch

---
+++ Sprinngbatch_frg/src/main/java/com/batch/TestSpriungBatch.java
@@ -0,0 +1,13 @@
+package com.batch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestSpriungBatch {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestSpriungBatch.class, args);
+	}
+
+}
